a35fcd68008a2d5ce3f3b23b1e0af411df9f7dfe
pcsteppan <patrick.steppan@gmail.com>
2024-12-30 19:23:38 -0600

    starting data structure implementation

--- 19_ocaml/day19/bin/main.ml
+++ 19_ocaml/day19/bin/main.ml
@@ -1,1 +1,21 @@
-let () = print_endline "Hello, World!"
+
+module Trie = struct
+  type node = Node of char * bool * node list
+  type 'a trie = node list
+
+  let empty = []
+
+  let from_word (word: string) : 'a trie =
+    let letters = String.to_seq word |> List.of_seq in
+    let rec aux chars : node option =
+      match chars with
+      | [] -> None
+      | c :: remaining -> match (aux remaining) with
+        | Some n -> Some (Node (c, false, [n]))
+        | None -> Some (Node (c, true, []))
+    in
+    [aux letters |> Option.get]
+
+end
+
+let test = Trie.from_word "abc"
